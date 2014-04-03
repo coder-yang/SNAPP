bb10bca34c3ee79052c823c5f0f5c465b0343ca1
simon <451493485@qq.com>
2014-04-03 14:17:55 +0800

    add new files

---
+++ SinaweiboDemo/Controllers/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  SinaweiboDemo
+//
+//  Created by Simon on 14-4-3.
+//  Copyright (c) 2014年 Simon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
