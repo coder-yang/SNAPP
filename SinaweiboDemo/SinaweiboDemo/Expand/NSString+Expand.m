f8f34d45141a7625896d0fbd9e1890a0745e5063
yangfm <451493485@qq.com>
2015-09-25 11:48:48 +0800

    -

--- SinaweiboDemo/SinaweiboDemo/Expand/NSString+Expand.m
+++ SinaweiboDemo/SinaweiboDemo/Expand/NSString+Expand.m
@@ -79,45 +79,9 @@ const Byte iv[] = {1,2,3,4,5,6,7,8};
 //判断字符串是否为手机号码
 + (BOOL)isMobileNumber:(NSString *)mobileNum
 {
-    /**
-     * 手机号码
-     * 移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
-     * 联通：130,131,132,152,155,156,185,186
-     * 电信：133,1349,153,180,189
-     */
-    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
-    /**
-     10         * 中国移动：China Mobile
-     11         * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
-     12         */
-    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
-    /**
-     15         * 中国联通：China Unicom
-     16         * 130,131,132,152,155,156,185,186
-     17         */
-    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
-    /**
-     20         * 中国电信：China Telecom
-     21         * 133,1349,153,180,189
-     22         */
-    //    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
-    NSString * CT = @"^1((33|53|8[019])[0-9]|349)\\d{7}$"; //新增 181
-    /**
-     25         * 大陆地区固话及小灵通
-     26         * 区号：010,020,021,022,023,024,025,027,028,029
-     27         * 号码：七位或八位
-     28         */
-    // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
-    
-    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
-    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
-    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
-    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
-    
-    if (([regextestmobile evaluateWithObject:mobileNum] == YES)
-        || ([regextestcm evaluateWithObject:mobileNum] == YES)
-        || ([regextestct evaluateWithObject:mobileNum] == YES)
-        || ([regextestcu evaluateWithObject:mobileNum] == YES))
+    NSString * number = @"\\d{7,11}";
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", number];
+    if (([regextestmobile evaluateWithObject:mobileNum] == YES))
     {
         return YES;
     }
@@ -278,7 +242,7 @@ const Byte iv[] = {1,2,3,4,5,6,7,8};
 
 - (int)getWidthByHeight:(float)aHeight font:(UIFont *)aFont
 {
-    CGRect rect = [self boundingRectWithSize:CGSizeMake(LONG_MAX, 20)
+    CGRect rect = [self boundingRectWithSize:CGSizeMake(LONG_MAX, aHeight)
                                      options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
                                   attributes:@{NSFontAttributeName:aFont}
                                      context:nil];
